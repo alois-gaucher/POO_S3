bba7ea60e7d6ba8843ab22d5c18fcb4f6b69ff14
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-02-10 14:57:21 +0100

    Tous les exercices terminés et fonctionnels

---
+++ 1. Révisions/td1_tableau_2d.php
@@ -0,0 +1,20 @@
+<?php
+// Déclaration d'un tableau 2 dimension
+$tableau_notes = array();
+// Ajout de données,$tableau_notes est un tableau de tableau
+$tableau_notes[0] = array('Pierre',14);
+$tableau_notes[1] = array('Paul',10);
+$tableau_notes[2] = array('Jacques',12);
+
+// Afficher la note de Paul
+echo 'Note de Paul: '.$tableau_notes[1][1].'';
+
+// Boucle de lecture
+foreach ($tableau_notes as $etudiant) {
+    // $etudiant est un tableau
+    echo $etudiant[0]; // nom de l'étudiant
+    echo ' : ';
+    echo $etudiant[1] ; // note de l'étudiant
+  echo '<br>';
+}
+?>
